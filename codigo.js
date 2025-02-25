e6758b63320209b044b3f5d7a7d8556d1db35846
robertocarlosfemboy <tdesenvolvimentos@gmail.com>
2025-02-24 21:44:12 -0300

    Add files via upload
    aula do dia 24

---
+++ codigo.js
@@ -0,0 +1,69 @@
+/*console.log("oi");
+var num = 10;
+if (num %2 == 0 ){
+    console.log("O numero é par");
+}
+else{
+    console.log("seu numero é impar");
+}
+var y;
+contador = 0;
+for(y=1;y <= num; y++ ){
+    if(num %y == 0){
+        contador ++;
+    }
+}
+if (contador ==2){
+    console.log("é primo")
+}
+else{
+    console.log("não é primo")
+}      */             
+
+/*var opcao = 0;
+var num1 = 0;
+var num2 = 0;
+var resultado =0
+if (opcao == 0){
+    console.log("")
+    (resultado + opcao )
+}
+
+if (opcao == 1){
+
+}
+if (opcao == 2){
+
+}
+if (opcao == 3){
+
+}
+if (opcao == 4){
+
+}
+else{
+    /*console.log("opção é invalida") 
+} */
+
+
+
+var x = 0;
+var y = 1;
+var resultado;
+var quantidade = 8;
+resultado = x + y;
+var i = 3;
+while (i < quantidade ){
+    x = y;
+    y = resultado;
+    resultado = x+y;
+    i ++;
+}
+
+alert(resultado);
+
+
+
+
+
+
