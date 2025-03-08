ffce96d7cdca5de955c87a918ff784eeec9793f7
nick_0016 <tdesenvolvimentos@gmail.com>
2025-03-07 21:53:52 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -79,3 +79,39 @@ var estadocivil = prompt.str("Insira o sexo: ")
 if (nome.len < 3){
     
 };
+
+let conta = {
+    nome: "Hubiscleidown",
+    numero: "12345-6",
+    saldo: 0
+};
+
+//função para depositar
+function depositar(valor) {
+    if (valor > 1000) { 
+        console.log("Depósito máximo permitido é R$ 1000,00.")
+    } else {
+        console.log("Depósito de R$" + valor + "efetuado. Saldo atual: R$" + saldo);
+    }
+}
+//função para sacar
+function sacar(valor){
+    if (valor > saldo) {
+        console.log("Saldo insuficiente.");
+    } else {
+        console.log -= valor;("Saque de R$" + valor + "realizado. Saldo atual: R$"  + saldo);
+    }
+}
+
+//função para verificar saldo
+function verificarSaldo() {
+    console.log("Saldo atual: R$" + saldo);
+}
+
+sacar()
+function operacao() {
+    if () {
+        
+    }
+}
+
