8887f0130488ef32213ab43cc900379fc180dc95
nick_0016 <tdesenvolvimentos@gmail.com>
2025-03-28 21:50:29 -0300

    Create Nicoli.java

---
+++ Nicoli.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nicoli;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author CEDUP16
+ */
+public class Nicoli {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        Scanner teclado = new Scanner(System.in);
+        
+        //Escreva um programa em java que imprima olá e o nome que o usuário digitar.
+        /*System.out.print("Digite o nome: ");
+        String nome = teclado.nextLine();
+        System.out.print("Olá\n");
+        System.out.println(nome);*/
+        
+        //Escreva um programa que some dois números escritos pelo usuário.
+        /*float num1 = teclado.nextFloat();
+        float num2 = 3.5f;
+        float resultado = num1 + num2;
+        System.out.print(num1 + " + " + num2 + " = " + resultado);*/
+        
+        //Escreva um programa que divide dois números escritos pelo usuário.
+        int num1 = teclado.nextInt("Coloque o primeiro número: ");
+        int num2 = teclado.nextInt("Coloque o segundo número: ");
+        float resultado = num1 / num2;
+        System.out.print(num1 + " / " + num2 + " = " + resultado);
+        
+        //Escreva um programa que multiplica dois números escritos pelo usuário.
+        int num1 = 
+        
+        
+        
+    }   
+}
+
+
